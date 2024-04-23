4ffc09a3a3d5e4249e904113a8a5a70f128470b8
Sebastián Torres <sebastorres325@gmail.com>
2024-04-23 01:35:21 -0500

    creacion primer ejemplo facade

---
+++ patron_facade/src/main/java/patron_facade/App.java
@@ -0,0 +1,16 @@
+package patron_facade;
+
+import patron_facade.facade.CheckFacade;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        CheckFacade cliente1 = new CheckFacade();
+        cliente1.buscar("02/07/2018", "08/07/2018", "Lima", "Cancún");
+
+        CheckFacade cliente2 = new CheckFacade();
+        cliente2.buscar("02/07/2018", "08/07/2018", "Lima", "Quito");
+    }
+
+}
